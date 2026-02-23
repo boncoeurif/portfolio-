624611a68c7f2937a607dc568611dc192dea8830
boncoeurif <irakozefabriceboncoeur@gmail.com>
2026-02-23 14:18:37 +0200

    3commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import Skill from '@/views/Skill.vue'
+import Skill from '@/views/SkillView.vue'
 import Projects from '@/views/Projects.vue'
 import Contact from '@/views/Contact.vue'
 import Blog from '@/views/Blog.vue' // New import
